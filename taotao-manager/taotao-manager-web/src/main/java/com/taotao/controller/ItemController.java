fe9262e03c27d099a9b96f0921d190fa408ea37b
nnngu <654015592@qq.com>
2018-03-06 23:00:37 +0800

    服务调用测试

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -0,0 +1,22 @@
+package com.taotao.controller;
+
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ItemController {
+
+    @Autowired
+    public ItemService itemService;
+
+    @RequestMapping("/item/{itemId}")
+    @ResponseBody
+    public TbItem getItemById(@PathVariable Long itemId) {
+        return itemService.getItemById(itemId);
+    }
+}
