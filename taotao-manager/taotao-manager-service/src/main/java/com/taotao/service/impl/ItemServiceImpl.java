2babe7922bcad7ddcd22e2100879a23467be91b0
nnngu <654015592@qq.com>
2018-03-06 19:25:37 +0800

    发布dubbo服务

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,20 @@
+package com.taotao.service.impl;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private TbItemMapper tbItemMapper;
+
+    @Override
+    public TbItem getItemById(long itemId) {
+        TbItem item = tbItemMapper.selectByPrimaryKey(itemId);
+        return item;
+    }
+}
